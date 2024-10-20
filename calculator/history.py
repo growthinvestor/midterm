9dc24fbe66bfbe822ae0013228f72c3a64203554
growthinvestor <amanjaswal007@gmail.com>
2024-10-19 23:13:23 -0400

    Updated history file to capture more parts

--- calculator/history.py
+++ calculator/history.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import os  # Import the os module
 
 class HistoryManager:
     def __init__(self, history_file):
         self.history_file = history_file
         self.history = pd.DataFrame(columns=["operation", "result"])
+        self.ensure_history_file_exists()
+
+    def ensure_history_file_exists(self):
+        # Get the directory name from the history file path
+        directory = os.path.dirname(self.history_file)
+        # Create the directory if it doesn't exist
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
     def load_history(self):
         try:
@@ -19,7 +28,10 @@ class HistoryManager:
         self.save_history()
 
     def add_record(self, operation, result):
-        self.history = self.history.append({"operation": operation, "result": result}, ignore_index=True)
+        # Create a new DataFrame to append
+        new_record = pd.DataFrame({"operation": [operation], "result": [result]})
+        # Use pd.concat to add the new record to the history DataFrame
+        self.history = pd.concat([self.history, new_record], ignore_index=True)
         self.save_history()
 
     def get_history(self):
