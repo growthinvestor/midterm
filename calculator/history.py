ae0c4d5e526b251d6fb6889387bb59678ea3a763
growthinvestor <amanjaswal007@gmail.com>
2024-10-19 21:23:50 -0400

    Made changes to history.py

--- calculator/history.py
+++ calculator/history.py
@@ -1,22 +1,26 @@
 import pandas as pd
-import os
 
-HISTORY_FILE = 'calculation_history.csv'
+class HistoryManager:
+    def __init__(self, history_file):
+        self.history_file = history_file
+        self.history = pd.DataFrame(columns=["operation", "result"])
 
-def load_history():
-    if os.path.exists(HISTORY_FILE):
-        return pd.read_csv(HISTORY_FILE)
-    return pd.DataFrame(columns=['operation', 'result'])
+    def load_history(self):
+        try:
+            self.history = pd.read_csv(self.history_file)
+        except FileNotFoundError:
+            self.history = pd.DataFrame(columns=["operation", "result"])
 
-def save_history(operation, result):
-    df = load_history()
-    new_record = pd.DataFrame([[operation, result]], columns=['operation', 'result'])
-    df = pd.concat([df, new_record])
-    df.to_csv(HISTORY_FILE, index=False)
+    def save_history(self):
+        self.history.to_csv(self.history_file, index=False)
 
-def clear_history():
-    if os.path.exists(HISTORY_FILE):
-        os.remove(HISTORY_FILE)
+    def clear_history(self):
+        self.history = pd.DataFrame(columns=["operation", "result"])
+        self.save_history()
 
-def get_history():
-    return load_history()
+    def add_record(self, operation, result):
+        self.history = self.history.append({"operation": operation, "result": result}, ignore_index=True)
+        self.save_history()
+
+    def get_history(self):
+        return self.history
